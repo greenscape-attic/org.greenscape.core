b4ac3dd0fca07c53c81a4622d55355cbb53f5ef0
smsajid <smsajid786@gmail.com>
2014-07-13 18:07:59 +0530

    #1 - copied organization and site related class to this bundle - add the model class names to bundle manifest

---
+++ src/org/greenscape/core/model/Organization.java
@@ -0,0 +1,83 @@
+package org.greenscape.core.model;
+
+import org.greenscape.persistence.PersistedModelBase;
+import org.greenscape.persistence.annotations.Model;
+
+/**
+ * 
+ * @author Sheikh Sajid
+ * 
+ */
+@Model(name = OrganizationModel.MODEL_NAME)
+public class Organization extends PersistedModelBase implements OrganizationModel {
+	private static final long serialVersionUID = -2392049292137025175L;
+
+	public Organization() {
+		getPropertyNames().add(NAME);
+		getPropertyNames().add(HOME_URL);
+		getPropertyNames().add(LOGO_ID);
+		getPropertyNames().add(MAX_USERS);
+		getPropertyNames().add(ACTIVE);
+	}
+
+	public Organization(Organization entity) {
+		this();
+		getProperties().putAll(entity.getProperties());
+	}
+
+	@Override
+	public String getName() {
+		return (String) getProperty(NAME);
+	}
+
+	@Override
+	public OrganizationModel setName(String name) {
+		setProperty(NAME, name);
+		return this;
+	}
+
+	@Override
+	public String getHomeURL() {
+		return (String) getProperty(HOME_URL);
+	}
+
+	@Override
+	public OrganizationModel setHomeURL(String homeURL) {
+		setProperty(HOME_URL, homeURL);
+		return this;
+	}
+
+	@Override
+	public Long getLogoId() {
+		return (Long) getProperty(LOGO_ID);
+	}
+
+	@Override
+	public OrganizationModel setLogoId(Long logoId) {
+		setProperty(LOGO_ID, logoId);
+		return this;
+	}
+
+	@Override
+	public Integer getMaxUsers() {
+		return (Integer) getProperty(MAX_USERS);
+	}
+
+	@Override
+	public OrganizationModel setMaxUsers(Integer maxUsers) {
+		setProperty(MAX_USERS, maxUsers);
+		return this;
+	}
+
+	@Override
+	public Boolean isActive() {
+		return (Boolean) getProperty(ACTIVE);
+	}
+
+	@Override
+	public OrganizationModel setActive(Boolean active) {
+		setProperty(ACTIVE, active);
+		return this;
+	}
+
+}
